69afbadf0290fed4b373b695d98040578dc3e66b
iv <consmebius@gmail.com>
2023-03-15 16:32:35 +0200

    w

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  getHello(): { version: any } {
+  getHello(): { version: number } {
     return { version: process.env.npm_package_version };
   }
 }
